bcb25a7fc775eb7bf50ee58be8667d95954d6f7c
Jack Gilbride <jackgilbride999@gmail.com>
2020-05-13 14:42:43 +0100

    Updated test harness

--- csu33014-annual-partB/csu33014-annual-partB-main.c
+++ csu33014-annual-partB/csu33014-annual-partB-main.c
@@ -70,32 +70,67 @@ void initialize_random() {
   srandom(seed);
 }
 
+// get the difference in microseconds between two times
+long long time_diff(struct timeval start, struct timeval stop) {
+  long long result = (stop.tv_sec - start.tv_sec) * 1000000L +
+    (stop.tv_usec - start.tv_usec);
+  return result;
+}
+
 // main function to test the code
-int main() {
+int main(int argc, char ** argv) {
   struct person ** graph;
-  int count_correct, count_reduced, count_parallel;
+  int count_warmup, count_correct, count_reduced, count_parallel;
+  struct timeval start_time;
+  struct timeval stop_time;
+  long long compute_time;
+
+  if ( argc != 3 ) {
+    fprintf(stderr, "Usage: ./graph <nodes> <k>\n");
+    exit(1);
+  }
+
+  int nnodes = atoi(argv[1]);
+  int k = atoi(argv[2]);
   
   // initialize the pseudo-random number generator
   initialize_random();
 
   // create a random graph of people
-  const int nnodes = 8357;
   const int nedges = nnodes * 2;
   graph = gen_random_people_graph(nnodes, nedges);
 
+  // compute the function once to warm up the processor's caches
+  const bool warmup = false;
+  if ( warmup ) {
+    gettimeofday(&start_time, NULL);
+    count_warmup = number_within_k_degrees(graph[0], nnodes, k);
+    gettimeofday(&stop_time, NULL);
+    compute_time = time_diff(start_time, stop_time);
+    fprintf(stderr, "Warm up count %d, Time: %lld\n", count_warmup, compute_time);
+  }
+  
+  
   // count the number of people reachable from node zero within k steps
-  const int k = 6;
+  gettimeofday(&start_time, NULL);
   count_correct = number_within_k_degrees(graph[0], nnodes, k);
+  gettimeofday(&stop_time, NULL);
+  compute_time = time_diff(start_time, stop_time);
+  fprintf(stderr, "Correct count %d, Time: %lld\n", count_correct, compute_time);
 
   // check the student's version that reduces redundant work
+  gettimeofday(&start_time, NULL);
   count_reduced = less_redundant_number_within_k_degrees(graph[0], nnodes, k);
+  gettimeofday(&stop_time, NULL);
+  compute_time = time_diff(start_time, stop_time);
+  fprintf(stderr, "Less redundant count %d, Time: %lld\n", count_reduced, compute_time);
   
   // check the student's parallel version
+  gettimeofday(&start_time, NULL);
   count_parallel = parallel_number_within_k_degrees(graph[0], nnodes, k);
-
-  fprintf(stderr,
-	  "Correct count %d, Reduced redundancy count: %d, Parallel count %d\n",
-	  count_correct, count_reduced, count_parallel);
+  gettimeofday(&stop_time, NULL);
+  compute_time = time_diff(start_time, stop_time);
+  fprintf(stderr, "Parallel count %d, Time: %lld\n", count_parallel, compute_time);
   
   return 0;
 }
